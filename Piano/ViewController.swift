fbcc69df13260db9c4a7a473e792acc906b21693
Vadym <markov.vadym@gmail.com>
2018-11-27 23:55:34 +0100

    Initial Commit

---
+++ Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Piano
+//
+//  Created by Vadym Markov on 27/11/2018.
+//  Copyright © 2018 FINN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
